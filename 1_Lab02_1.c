fa88465a3fe3f0bdffb3cf09486e073705db54c9
songun <songun13655@sookmyung.ac.kr>
2026-04-06 22:31:08 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,28 @@
+#if 0
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B, result;
+
+	scanf("%d %d", &A, &B);
+
+	result = A + B;
+	printf("%d\n", result);
+
+	result = A - B;
+	printf("%d\n", result);
+
+	result = A * B;
+	printf("%d\n", result);
+
+	result = A / B;
+	printf("%d\n", result);
+
+	result = A % B;
+	printf("%d\n", result);
+
+	return 0;
+}
+#endif
